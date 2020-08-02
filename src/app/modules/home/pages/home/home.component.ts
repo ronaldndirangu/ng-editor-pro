e4a56646178296d07578e110b1d6d97af81f964c
Ronald Ndirangu <ronald.ndirangu@gmail.com>
2020-08-02 03:24:31 +0300

    feat: add medium editor

--- src/app/modules/home/pages/home/home.component.ts
+++ src/app/modules/home/pages/home/home.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild, ElementRef } from '@angular/core';
+import MediumEditor from 'medium-editor';
 
 @Component({
   selector: 'app-home',
@@ -6,10 +7,22 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home.component.scss']
 })
 export class HomeComponent implements OnInit {
+  editor: any;
+
+  @ViewChild('editable', { static: true }) editable: ElementRef;
 
   constructor() { }
 
   ngOnInit() {
+    this.editor = new MediumEditor(this.editable.nativeElement, {
+      delay: 1000,
+      placeholder: {
+        text: 'Start typing here...',
+        hideOnClick: true
+      }
+    }).subscribe('editableKeyup', (evt) => {
+      console.log({ html: evt.target.innerHTML });
+    });
   }
 
 }
